613ba051c63d712cebf70fd3297934e0400d6401
basava-chainflux <basava@chainflux.com>
2022-07-13 17:25:32 +0530

    Added Validation

--- src/Components/SignIn.js
+++ src/Components/SignIn.js
@@ -10,72 +10,92 @@ import Typography from "@mui/material/Typography";
 // MUI Utilities
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
+import { Snackbar } from "@mui/material";
 
 const SignIn = () => {
-    const userArray = useSelector((state) => state.MainData.userArray);
-    const navigate = useNavigate();
-  
-    const handleSubmit = (event) => {
-      event.preventDefault();
-      const data = new FormData(event.currentTarget);
-  
-      userArray.map((item) => {
-        if (
-          item.email === data.get("email") &&
-          item.password === data.get("password")
-        ) {
-          navigate("/Dashboard");
-        }
-      });
-    };
-  
-    return (
-      <Box
-        sx={{
-          marginTop: 8,
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-        }}
-      >
-        <Typography component="h1" variant="h5">
-          Sign in
-        </Typography>
-        <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
-          <TextField
-            margin="normal"
-            required
-            fullWidth
-            id="email"
-            label="Email Address"
-            name="email"
-            autoComplete="email"
-            variant="standard"
-            autoFocus
-            // color="secondary"
-          />
-          <TextField
-            margin="normal"
-            required
-            fullWidth
-            name="password"
-            label="Password"
-            type="password"
-            id="password"
-            variant="standard"
-            autoComplete="current-password"
-          />
-          <Button
-            type="submit"
-            fullWidth
-            variant="outlined"
-            sx={{ mt: 3, mb: 2 }}
-          >
-            Sign In
-          </Button>
-        </Box>
-      </Box>
-    );
+  const [open, setOpen] = React.useState(false);
+  const [snackBarText, setSnackBarText] = React.useState("Login Successful!");
+  const userArray = useSelector((state) => state.MainData.userArray);
+  const navigate = useNavigate();
+  let loginSuccess = false;
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+
+    userArray.map((item) => {
+      if (
+        item.email === data.get("email") &&
+        item.password === data.get("password")
+      ) {
+        loginSuccess = true;
+      }
+    });
+
+    if (loginSuccess) {
+      setOpen(true);
+      setSnackBarText("Login Successful!");
+      setTimeout(() => {
+        setOpen(false);
+        navigate("/Dashboard");
+      }, 1000);
+    } else {
+      setOpen(true);
+      setSnackBarText("Invalid Credentials!");
+      setTimeout(() => {
+        setOpen(false);
+      }, 3000);
+    }
   };
 
-  export default SignIn
+  return (
+    <Box
+      sx={{
+        marginTop: 8,
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+      }}
+    >
+      <Typography component="h1" variant="h5">
+        Sign in
+      </Typography>
+      <Snackbar open={open} message={snackBarText} />
+      <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+        <TextField
+          margin="normal"
+          required
+          fullWidth
+          id="email"
+          label="Email Address"
+          name="email"
+          autoComplete="email"
+          variant="standard"
+          autoFocus
+          // color="secondary"
+        />
+        <TextField
+          margin="normal"
+          required
+          fullWidth
+          name="password"
+          label="Password"
+          type="password"
+          id="password"
+          variant="standard"
+          autoComplete="current-password"
+        />
+        <Button
+          type="submit"
+          fullWidth
+          variant="outlined"
+          sx={{ mt: 3, mb: 2 }}
+        >
+          Sign In
+        </Button>
+      </Box>
+    </Box>
+  );
+};
+
+export default SignIn;
--- src/Components/SignUp.js
+++ src/Components/SignUp.js
@@ -13,81 +13,99 @@ import { AddUser } from "../Redux/Actions/MainActions";
 import { useSelector } from "react-redux";
 
 const SignUp = () => {
-    const [open, setOpen] = React.useState(false);
-  
-    const handleSubmit = (event) => {
-      event.preventDefault();
-      const data = new FormData(event.currentTarget);
+  const [open, setOpen] = React.useState(false);
+  const [snackBarText, setSnackBarText] = React.useState(
+    "Successfully Added the User! Now you can use these credentials to Login"
+  );
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+
+    if (
+      data.get("fullname") !== "" &&
+      data.get("email") !== "" &&
+      data.get("password") !== ""
+    ) {
       AddUser(data.get("fullname"), data.get("email"), data.get("password"));
       setOpen(true);
+      setSnackBarText(
+        "Successfully Added the User! Now you can use these credentials to Login"
+      );
       setTimeout(() => {
         setOpen(false);
       }, 3000);
-    };
-  
-    return (
-      <Box
-        sx={{
-          marginTop: 8,
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-        }}
-      >
-        <Typography component="h1" variant="h5">
-          Sign Up
-        </Typography>
-        <Snackbar
-          open={open}
-          message="Successfully Added the User! Now you can use these credentials to Login"
-        />
-        <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
-          <TextField
-            margin="normal"
-            required
-            fullWidth
-            id="fullname"
-            label="Full Name"
-            name="fullname"
-            autoComplete="fullname"
-            variant="standard"
-            autoFocus
-            // color="secondary"
-          />
-          <TextField
-            margin="normal"
-            required
-            fullWidth
-            id="email"
-            label="Email Address"
-            name="email"
-            autoComplete="email"
-            variant="standard"
-            autoFocus
-            // color="secondary"
-          />
-          <TextField
-            margin="normal"
-            required
-            fullWidth
-            name="password"
-            label="Password"
-            type="password"
-            id="password"
-            variant="standard"
-            autoComplete="current-password"
-          />
-          <Button
-            type="submit"
-            fullWidth
-            variant="outlined"
-            sx={{ mt: 3, mb: 2 }}
-          >
-            Sign Up
-          </Button>
-        </Box>
-      </Box>
-    );
+    } else {
+      setOpen(true);
+      setSnackBarText(
+        "Fill all the fields"
+      );
+      setTimeout(() => {
+        setOpen(false);
+      }, 3000);
+    }
   };
 
-  export default SignUp
+  return (
+    <Box
+      sx={{
+        marginTop: 8,
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+      }}
+    >
+      <Typography component="h1" variant="h5">
+        Sign Up
+      </Typography>
+      <Snackbar open={open} message={snackBarText} />
+      <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+        <TextField
+          margin="normal"
+          required
+          fullWidth
+          id="fullname"
+          label="Full Name"
+          name="fullname"
+          autoComplete="fullname"
+          variant="standard"
+          autoFocus
+          // color="secondary"
+        />
+        <TextField
+          margin="normal"
+          required
+          fullWidth
+          id="email"
+          label="Email Address"
+          name="email"
+          autoComplete="email"
+          variant="standard"
+          autoFocus
+          // color="secondary"
+        />
+        <TextField
+          margin="normal"
+          required
+          fullWidth
+          name="password"
+          label="Password"
+          type="password"
+          id="password"
+          variant="standard"
+          autoComplete="current-password"
+        />
+        <Button
+          type="submit"
+          fullWidth
+          variant="outlined"
+          sx={{ mt: 3, mb: 2 }}
+        >
+          Sign Up
+        </Button>
+      </Box>
+    </Box>
+  );
+};
+
+export default SignUp;
